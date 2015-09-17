85ae33dbdf31cbed732773bb72a3bfc306185983
Sam Eubank <sameubank+zerious@gmail.com>
2015-09-17 08:24:24 -0700

    Initial commit

---
+++ test/stringify-bench.js
@@ -0,0 +1,16 @@
+var fs = require('fs')
+var stringify = require(__dirname + '/lighter-stringify')
+
+describe('Benchmarks', function () {
+
+  bench('', function () {
+
+    it('', function () {
+    })
+
+    it('', function () {
+    })
+
+  })
+
+})
---
+++ test/stringify-test.js
@@ -0,0 +1,26 @@
+var stringify = require('../lighter-stringify')
+var is = global.is || require('exam/lib/is')
+
+describe('stringify', function () {
+
+  describe('', function () {
+
+    it('', function () {
+    })
+
+    it('', function () {
+    })
+
+  })
+
+  describe('', function () {
+
+    it('', function () {
+    })
+
+    it('', function () {
+    })
+
+  })
+
+})
